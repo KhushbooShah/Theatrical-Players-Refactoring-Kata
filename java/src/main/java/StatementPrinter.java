2131ef4f696f31379819093ae2c6f21f381211c7
KhushbooShah <shahkhushboo0@gmail.com>
2022-05-25 22:26:57 -0400

    Extracting switch case logic to calculate amount based on play type

--- java/src/main/java/StatementPrinter.java
+++ java/src/main/java/StatementPrinter.java
@@ -12,23 +12,7 @@ public class StatementPrinter {
             var play = plays.get(perf.getPlayID());
             var thisAmount = 0;
 
-            switch (play.getType()) {
-                case "tragedy":
-                    thisAmount = 40000;
-                    if (perf.getAudience() > 30) {
-                        thisAmount += 1000 * (perf.getAudience() - 30);
-                    }
-                    break;
-                case "comedy":
-                    thisAmount = 30000;
-                    if (perf.getAudience() > 20) {
-                        thisAmount += 10000 + 500 * (perf.getAudience() - 20);
-                    }
-                    thisAmount += 300 * perf.getAudience();
-                    break;
-                default:
-                    throw new Error("unknown type: "+play.getType());
-            }
+            thisAmount = calculateAmountBasedOnPlayType(perf, play, thisAmount);
 
             volumeCredits = calculateVolumeCredits(volumeCredits, perf, play);
 
@@ -39,6 +23,27 @@ public class StatementPrinter {
         result += String.format("Amount owed is %s\n", Currency.formatAmount(totalAmount / 100));
         result += String.format("You earned %s credits\n", volumeCredits);
         return result;
+    }
+
+    private int calculateAmountBasedOnPlayType(Performance perf, Play play, int thisAmount) throws Error {
+        switch (play.getType()) {
+            case "tragedy":
+                thisAmount = 40000;
+                if (perf.getAudience() > 30) {
+                    thisAmount += 1000 * (perf.getAudience() - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (perf.getAudience() > 20) {
+                    thisAmount += 10000 + 500 * (perf.getAudience() - 20);
+                }
+                thisAmount += 300 * perf.getAudience();
+                break;
+            default:
+                throw new Error("unknown type: "+play.getType());
+        }
+        return thisAmount;
     }
 
     private int calculateVolumeCredits(int volumeCredits, Performance perf, Play play) {
