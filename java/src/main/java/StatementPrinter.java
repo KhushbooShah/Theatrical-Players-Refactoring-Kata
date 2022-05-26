1002dd791a67419133c491c32ad0730d60e70f53
KhushbooShah <shahkhushboo0@gmail.com>
2022-05-25 22:59:50 -0400

    Renaming thisAmount to amount

--- java/src/main/java/StatementPrinter.java
+++ java/src/main/java/StatementPrinter.java
@@ -12,35 +12,35 @@ public class StatementPrinter {
 
         for (Performance perf : invoice.getPerformances()) {
             Play play = plays.get(perf.getPlayID());
-            Long thisAmount = 0L;
+            Long amount = 0L;
 
-            thisAmount = calculateAmountBasedOnPlayType(perf, play, thisAmount);
+            amount = calculateAmountBasedOnPlayType(perf, play, amount);
 
             volumeCredits = calculateVolumeCredits(volumeCredits, perf, play);
 
             // print line for this order
-            result += String.format("  %s: %s (%s seats)\n", play.getName(), Currency.formatAmount(thisAmount / 100), perf.getAudience());
-            totalAmount += thisAmount;
+            result += String.format("  %s: %s (%s seats)\n", play.getName(), Currency.formatAmount(amount / 100), perf.getAudience());
+            totalAmount += amount;
         }
         result += String.format("Amount owed is %s\n", Currency.formatAmount(totalAmount / 100));
         result += String.format("You earned %s credits\n", volumeCredits);
         return result;
     }
 
-    private Long calculateAmountBasedOnPlayType(Performance perf, Play play, Long thisAmount) throws Error {
+    private Long calculateAmountBasedOnPlayType(Performance perf, Play play, Long amount) throws Error {
         switch (play.getType()) {
             case "tragedy":
                 playType = new TragedyPlay();
-                thisAmount += playType.calculateAmount(perf.getAudience());
+                amount += playType.calculateAmount(perf.getAudience());
                 break;
             case "comedy":
                 playType = new ComedyPlay();
-                thisAmount += playType.calculateAmount(perf.getAudience());
+                amount += playType.calculateAmount(perf.getAudience());
                 break;
             default:
                 throw new Error("unknown type: "+play.getType());
         }
-        return thisAmount;
+        return amount;
     }
 
     private int calculateVolumeCredits(int volumeCredits, Performance perf, Play play) {
